09b1f30b518ec9f813244601fd7e1f218a09815a
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-07-22 16:45:21 -0400

    Create Menu.js

---
+++ my-project/frontend/src/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Menu = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <button onClick={() => navigate('/menu/tasks')}>Tasks</button>
+            <button onClick={() => navigate('/menu/chat')} >Chat</button>
+            <button onClick={() => navigate('/menu/store')} >Store</button>
+        </div>
+    );
+};
+
+export default Menu;
